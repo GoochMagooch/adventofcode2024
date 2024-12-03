256eb535e1a9c2d9a50050526f059303b67f5b3f
Magooch <crvallecito@gmail.com>
2024-12-03 04:07:05 -0700

    Unfinished

--- day2/day2.py
+++ day2/day2.py
@@ -1,3 +1,8 @@
 with open('input.txt') as levels:
-    data = levels.read()
+    data = levels.readlines()
 
+for line in data:
+    counter = 0
+    for i in line:
+        if i[counter] < i[counter + 1] & i[counter + 1] - i[counter] <= 3:
+            print(i)
