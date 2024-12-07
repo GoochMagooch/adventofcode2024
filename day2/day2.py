95d8a035d5ab348653c4405d137387de1c4dac26
Magooch <crvallecito@gmail.com>
2024-12-06 23:35:11 -0700

    Don't know what I was doing here

--- day2/day2.py
+++ day2/day2.py
@@ -1,8 +1,22 @@
 with open('input.txt') as levels:
-    data = levels.readlines()
+    data = levels.read()
 
+data_list = []
 for line in data:
-    counter = 0
-    for i in line:
-        if i[counter] < i[counter + 1] & i[counter + 1] - i[counter] <= 3:
-            print(i)
+    new_data = data.replace('\n', ',')
+    data_final = new_data.split(',')
+
+for item in data_final:
+    data_list.append(item)
+
+data_int_strings = []
+for item in data_list:
+    data_int_strings.append(item.split(' '))
+
+data_ints = []
+for item in data_int_strings:
+    ints_list = []
+    for i in item:
+        ints_list.append(int(i))
+    data_ints.append(ints_list)
+print(data_ints)
