2c4da93d416b3413921f2baff20c1db6a9f8f02d
Magooch <crvallecito@gmail.com>
2024-12-09 21:06:44 -0700

    Trying to do the challenge

--- day9/p1.py
+++ day9/p1.py
@@ -1,18 +1,34 @@
 with open("input.txt") as input:
     data = input.read()
 
+period = '.'
 ids = []
 counter = 0
 digit = 0
 for i in data:
-    id = []
     if counter % 2 == 0:
-        id.append(str(i) + " data block, " + "ID: " + str(digit))
+        ids.append(digit)
         counter += 1
-        ids.append(id)
-        id = []
         digit += 1
     else:
         counter += 1
         continue
-print(ids)
+
+free_space = []
+count = 0
+for i in data:
+    if count % 2 == 1:
+        free_space.append(i)
+        count += 1
+    else:
+        count += 1
+        continue
+
+# alterning through both above lists and adding to `id_free_space`
+# trying to turn the free space into periods of the same amount of free space
+id_free_space = []
+for i in range(len(ids)):
+    id_free_space.append("ID: " + str(ids[i]))
+    id_free_space.append("\." * free_space[i])
+
+print(id_free_space)
